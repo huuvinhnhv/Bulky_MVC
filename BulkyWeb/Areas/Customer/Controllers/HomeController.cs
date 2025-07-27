5f7b78c9a19ae2591a927b5a3bdf39a9c0d34b68
xuancuong-1711 <20880242.ddtx@gmail.com>
2025-07-27 21:55:01 +0700

    add home page and details

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
-using System.Diagnostics;
+using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BulkyBookWeb.Areas.Customer.Controllers
 {
@@ -8,15 +9,23 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(products);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            var product = _unitOfWork.Product.Get(x => x.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
